2ab34d2cd7384f91c6b8445ba9b43e745b92d2f6
SteveRuble <SteveRuble@users.noreply.github.com>
2020-01-27 16:39:59 -0500

    chore: tuning action

--- src/setup-bosun.ts
+++ src/setup-bosun.ts
@@ -1,5 +1,6 @@
 import * as installer from "./installer";
 import * as core from "@actions/core";
+import * as path from "path";
 
 async function run() {
   try {
@@ -7,7 +8,10 @@ async function run() {
 
     console.log(`Downloaded Bosun: ${bosunPath}`);
 
-    core.exportVariable("BOSUN_CONFIG", "../assets/bosun.yaml");
+    core.exportVariable(
+      "BOSUN_CONFIG",
+      path.join(__dirname, "/assets/bosun.yaml")
+    );
   } catch (error) {
     core.setFailed(error.message);
   }
